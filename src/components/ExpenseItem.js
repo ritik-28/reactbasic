244bcdddcf57cb38908bc475e02035c55b8e356f
ritik-28 <tiwari.ritik.1.108@gmail.com>
2023-08-17 13:10:50 +0530

    always inject element inside root element

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,9 @@
 function ExpenseItem() {
   return (
     <div>
+      <p>Food Rs 100</p>
+      <p>Petrol Rs 100</p>
+      <p>Movies Rs 200</p>
       <div>March 28th 2021</div>
       <div>
         <h2>Car Insuarence</h2>
