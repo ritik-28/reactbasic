c0d176f3c6fd9a5d048f56474e3b58f1cf1f1f56
ritik-28 <tiwari.ritik.1.108@gmail.com>
2023-08-24 18:09:28 +0530

    RenderingExpensesWithperformancoptimisations(keys)

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,39 +4,25 @@ import ExpenseDetails from "./ExpenseDetails";
 import React, { useState } from "react";
 
 const ExpenseItem = (props) => {
-  //   return React.createElement(
-  //     "div",
-  //     { className: "expense-item" },
-  //     React.createElement(ExpenseDate, { date: props.date }),
-  //     React.createElement(
-  //       "div",
-  //       {},
-  //       React.createElement("h1", {}, "expense 1"),
-  //       React.createElement("h1", {}, "expense 1")
-  //     )
-  //   );
   const [title, setTitle] = useState(props.title);
   const [amount, setAddExp] = useState(props.amount);
 
   const clickHandler = () => {
     setTitle("updated");
-    console.log(title);
   };
 
   const deleteExpenseHandler = (e) => {
     console.log(e.target.parentElement.remove());
-    console.log("this expense deleted");
   };
 
   const changeExpense = () => {
     setAddExp(amount + 100);
-    console.log("expenses increased by 100 dollers");
   };
 
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
-      <ExpenseDetails title={title} amount={amount} location={props.location} />
+      <ExpenseDetails title={title} amount={amount} />
       <button onClick={changeExpense}>Add $100</button>
       <button onClick={clickHandler}>changed title!!!!</button>
       <button onClick={deleteExpenseHandler}>Delete</button>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -5,8 +5,8 @@ import ExpenseForm from "./ExpenseForm";
 const NewExpense = (props) => {
   const saveExpenseDataHandler = (enterdExpenseData) => {
     const expenseData = {
-      ...enterdExpenseData,
       id: Math.random().toString(),
+      ...enterdExpenseData,
     };
     props.onAddExpense(expenseData);
   };
